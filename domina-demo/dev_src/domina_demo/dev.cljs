3dfca4473769483e0711a9e03c474496477fbfd6
Amir Rajan <ar@amirrajan.net>
2015-04-27 06:49:29 -0500

    added domina-demo

---
+++ domina-demo/dev_src/domina_demo/dev.cljs
@@ -0,0 +1,10 @@
+(ns domina-demo.dev
+    (:require
+     [domina-demo.core]
+     [figwheel.client :as fw]))
+
+(fw/start {
+  :websocket-url "ws://localhost:3449/figwheel-ws"
+  :on-jsload (fn []
+               ;; (stop-and-start-my app)
+               )})
---
+++ domina-demo/src/domina_demo/core.cljs
@@ -0,0 +1,28 @@
+(ns ^:figwheel-always domina-demo.core
+    (:require-macros [hiccups.core :as hiccups])
+    (:require
+     [domina :as dom]
+     [domina.css :as $] ;;https://github.com/levand/domina
+     [garden.core :refer [css]] ;;https://github.com/noprompt/garden
+     [hiccups.runtime :as hiccupsrt] ;;https://github.com/teropa/hiccups
+     ))
+
+(enable-console-print!)
+
+(hiccups/defhtml my-template []
+  [:div
+    [:a {:href "https://github.com/teropa/hiccups"} "hiccup"]])
+
+(hiccups/defhtml my-template-2 []
+  [:ul (for [x (range 1 4)] [:li x])])
+
+(hiccups/defhtml my-template-3 []
+  [:div#content.pretty "hello world"])
+
+(hiccups/defhtml styles []
+  [:style (css [:body {:font-family "Verdana"
+                       :font-size "20px"}])])
+
+(dom/destroy! ($/sel "#app >"))
+(dom/append! ($/sel "#app") (styles))
+(dom/append! ($/sel "#app") (my-template-2))
